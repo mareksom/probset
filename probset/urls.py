9be14754a6abf4b910cde607884741ba16484e15
Marek Sommer <mareksommer7@gmail.com>
2014-04-14 22:06:44 +0200

    Kod, który już coś robi.

--- probset/urls.py
+++ probset/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'probset.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+		url(r'$', 'probset.views.home', name='home'),
+
     url(r'^admin/', include(admin.site.urls)),
 )
---
+++ probset/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+def home(request):
+	return render(request, 'base.html', {})
