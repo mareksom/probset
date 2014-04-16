47b54c3524dfa92a7952a88fd328c989cf73fe83
Marek Sommer <mareksommer7@gmail.com>
2014-04-16 20:47:36 +0200

    Poprawka komentarzy

--- problems/urls.py
+++ problems/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, url
 from problems import views
 
 urlpatterns = patterns('',
-#	url(r'^(?P<ID>[0-9]+)/comments/(?P<page>[0-9]+)/$', views.comments, name='comments'),
 	url(r'^(?P<ID>[0-9]+)/comments/edit/(?P<comID>[0-9]+)/$', views.comments_edit, name='problems-problem-comments-edit'),
 	url(r'^(?P<ID>[0-9]+)/comments/add/$', views.comments_add, name='problems-problem-comments-add'),
+	url(r'^(?P<ID>[0-9]+)/comments/(?P<page>[0-9]+)/$', views.comments, name='problems-problem-comments'),
 	url(r'^(?P<ID>[0-9]+)/comments/$', views.comments, name='problems-problem-comments'),
 	url(r'^(?P<ID>[0-9]+)/task/$', views.task, name='problems-problem-task'),
 	url(r'^(?P<ID>[0-9]+)/edit/$', views.edit, name='problems-problem-edit'),
