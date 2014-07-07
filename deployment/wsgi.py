32e83eef24efb09384db907a2e2ce14c57dc22fe
Maciej Debski <winemore@staszic.waw.pl>
2014-07-07 20:33:25 +0200

    apache->deployment rename, uwsgi config fixes

--- deployment/wsgi.py
+++ deployment/wsgi.py
@@ -1,8 +1,5 @@
 import os, sys
 
-sys.path.append('/home/mareksom/www/probset')
-sys.path.append('/home/mareksom/www/probset/probset')
-
 os.environ['DJANGO_SETTINGS_MODULE'] = 'probset.settings'
 
 import django.core.handlers.wsgi
