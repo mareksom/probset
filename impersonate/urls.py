80dc8b60cf001245a39e1dfac14db935607c4357
mimi89999 <michel@lebihan.pl>
2020-07-20 15:18:35 +0200

    Remove django-impersonate from the repo

--- impersonate/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from impersonate.views import impersonate, stop_impersonate
-
-urlpatterns = [
-    url(r'^(?P<uid>\d+)/$', impersonate, name='impersonate-start'),
-    url(r'^stop/$', stop_impersonate, name='impersonate-stop'),
-]
